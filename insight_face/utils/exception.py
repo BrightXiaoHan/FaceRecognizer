5939ce545fbaf4789fac6b878fccac4e81fe7316
hanbing <beatmight@gmail.com>
2019-03-30 15:19:44 +0800

    define some exceptions

--- insight_face/utils/exception.py
+++ insight_face/utils/exception.py
@@ -1,5 +1,20 @@
 class EmptyTensorException(Exception):
-
+    """
+    Prevent possible error when dealing with empty tensor.
+    """
     pass
 
+
+class MultiFaceException(Exception):
+    """
+    Detect multiple face in a single image.
+    """
+    pass
+
+
+class NoSuchNameException(Exception):
+    """
+    Raised when specify a non-existing attribute by string
+    """
+    pass
 
